5c331ac26e1f303a65191f4703d254f6a8b72805
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-06-21 00:30:04 -0300

    criando classe para consumir API

--- src/main/java/br/com/projeto/tabelaFipe/TabelaFipeApplication.java
+++ src/main/java/br/com/projeto/tabelaFipe/TabelaFipeApplication.java
@@ -1,5 +1,6 @@
 package br.com.projeto.tabelaFipe;
 
+import br.com.projeto.tabelaFipe.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class TabelaFipeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+		Main main = new Main();
+		main.iniciaProjeto();
 	}
 }
---
+++ src/main/java/br/com/projeto/tabelaFipe/main/Main.java
@@ -0,0 +1,14 @@
+package br.com.projeto.tabelaFipe.main;
+
+import br.com.projeto.tabelaFipe.model.Menu;
+
+public class Main {
+    private final String ENDERECO_CARRO = "https://parallelum.com.br/fipe/api/v1/carros/marcas"
+
+    public void iniciaProjeto() {
+        Menu.titulo();
+
+
+
+    }
+}
