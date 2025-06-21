7977afc704743d00fcdb3f053e195f6c983177b6
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2025-06-21 01:11:32 -0300

    melhorando algumas classes e colocando a opção de caminhões e motos

--- src/main/java/br/com/projeto/tabelaFipe/main/Main.java
+++ src/main/java/br/com/projeto/tabelaFipe/main/Main.java
@@ -1,27 +1,46 @@
 package br.com.projeto.tabelaFipe.main;
 
-import br.com.projeto.tabelaFipe.model.DadosMarcasCarro;
+import br.com.projeto.tabelaFipe.model.DadosMarcas;
 import br.com.projeto.tabelaFipe.model.Menu;
 import br.com.projeto.tabelaFipe.service.ConsumoApi;
 import br.com.projeto.tabelaFipe.service.ConverteDados;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    private final String ENDERECO_CARRO = "https://parallelum.com.br/fipe/api/v1/carros/marcas";
+    private final String URL_FIPE = "https://parallelum.com.br/fipe/api/v1/";
 
     public void iniciaProjeto() {
+        Scanner scanner = new Scanner(System.in);
         Menu.titulo();
+        Menu.menu();
+        int op = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println();
+
+
+        String endereco = null;
+        if (op == 1) {
+            endereco = "carros/marcas";
+        } else if (op == 2) {
+            endereco = "caminhoes/marcas";
+        } else if (op == 3) {
+            endereco = "motos/marcas";
+        } else {
+            System.out.println("Opção invalida");
+            System.exit(0);
+        }
+
 
         ConsumoApi consumoApi = new ConsumoApi();
-        String json = consumoApi.obterDadosApi(ENDERECO_CARRO);
-        System.out.println(json);
+        String json = consumoApi.obterDadosApi(URL_FIPE + endereco);
 
         ConverteDados converteDados = new ConverteDados();
-        List<DadosMarcasCarro> dadosMarcasCarro = converteDados.obterDadosLista(json, DadosMarcasCarro.class);
+        List<DadosMarcas> dadosMarcasCarro = converteDados.obterDadosLista(json, DadosMarcas.class);
         dadosMarcasCarro.stream()
-                .sorted(Comparator.comparing(DadosMarcasCarro::codigo))
+                .sorted(Comparator.comparing(DadosMarcas::codigo))
                 .forEach(c -> System.out.println(String.format("Código: %s  " +
                         "Marca: %s", c.codigo(), c.marcaDoCarro())));
 
--- src/main/java/br/com/projeto/tabelaFipe/model/DadosMarcas.java
+++ src/main/java/br/com/projeto/tabelaFipe/model/DadosMarcas.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosMarcasCarro(@JsonAlias("codigo") String codigo,
-                               @JsonAlias("nome") String marcaDoCarro) {
+public record DadosMarcas(@JsonAlias("codigo") String codigo,
+                          @JsonAlias("nome") String marcaDoCarro) {
 }
